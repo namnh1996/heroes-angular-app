835ab012e705fc56860ab819e858e7708cae2bbf
namdev96 <namdev96.info@gmail.com>
2022-10-25 15:40:57 +0700

    completed show heroes list and show hero detail

---
+++ src/app/hero.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HEROES } from './mock-heors';
+import { Hero } from './hero';
+import { Observable, of } from 'rxjs';
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+  constructor() {}
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
+  }
+}
